8ebd76d694d1f3040d2944ead8f318ba30f241b6
Joel S <jschmitty@protonmail.com>
2023-04-18 11:14:40 -0500

    Added org mode configuration.

---
+++ lua/plugins/orgmode.lua
@@ -0,0 +1,27 @@
+-- since this is just an example spec, don't actually load anything here and return an empty spec
+-- stylua: ignore
+
+-- every spec file under config.plugins will be loaded automatically by lazy.nvim
+--
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+return {
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    build = ":TSUpdate",
+  },
+
+  {
+    "nvim-orgmode/orgmode",
+    ft = { "org" },
+    config = function()
+      require("orgmode").setup({})
+    end
+  },
+}
